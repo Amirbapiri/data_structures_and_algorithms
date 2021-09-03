50df98cd271eeeba1e21e017b02d77b227054098
amirbapiri <amirbapiri@gmail.com>
2021-09-03 22:50:20 +0430

    create insertion method for doubly linked list

---
+++ doubly_linked_list/main.py
@@ -0,0 +1,56 @@
+from enum import Enum
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    def insert(self, value, location):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            if location == 0:
+                node.next = self.head
+                self.head.prev = node
+                self.head = node
+            elif location == -1:
+                self.tail.next = node
+                node.prev = self.tail
+                self.tail = node
+            else:
+                index = 0
+                pointer = self.head
+                while index < location - 1:
+                    pointer = pointer.next
+                    index += 1
+                node.next = pointer.next
+                node.prev = pointer
+                node.next.prev = node
+                pointer.next = node
+
+
+if __name__ == "__main__":
+    d = DoublyLinkedList()
+
+    print([node.value for node in d])
+    d.insert(5, 0)
+    d.insert(0, 0)
+    d.insert(2, 1)
+    d.insert(6, 2)
+    print([node.value for node in d])
